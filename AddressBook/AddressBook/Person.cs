872a013d26257d671cba443b47622496d90e210d
nehak6296 <nehak6296@gmail.com>
2021-03-10 21:09:00 +0530

    [Neha]Add:Created Contact

---
+++ AddressBook/AddressBook/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Person
+    {
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public static int Zip  { get; set; }
+        public static long PhoneNumber { get; set; }        
+        public string Email { get; set; }          
+                
+    }
+    
+}
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -7,6 +7,29 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to address book program");
+            Program p = new Program();
+            p.AddContact();
+        }
+
+        public void AddContact()
+        {
+            Person person = new Person();
+            Console.WriteLine("Enter First Name");
+            person.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            person.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            person.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            person.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            person.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            AddressBook.Person.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number");
+            AddressBook.Person.PhoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email");
+            person.Email = Console.ReadLine();
         }
     }
 }
